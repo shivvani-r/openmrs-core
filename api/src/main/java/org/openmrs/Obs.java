a24cfe9a2bd83fdd97b34fd5b01b6979e10ef9e3
shivvani-r <shivaniramadugu@yahoo.com>
2026-08-03 13:08:56 +0530

    TRUNK-6739: Add ObsArchive Entity and Hibernate Configuration

--- api/src/main/java/org/openmrs/Obs.java
+++ api/src/main/java/org/openmrs/Obs.java
@@ -21,6 +21,7 @@ import java.util.Locale;
 import java.util.Set;
 
 import org.apache.commons.lang3.StringUtils;
+import org.hibernate.ObjectNotFoundException;
 import org.hibernate.envers.Audited;
 import org.openmrs.annotation.AllowDirectAccess;
 import org.openmrs.api.APIException;
@@ -163,6 +164,8 @@ public class Obs extends BaseFormRecordableOpenmrsData {
 	protected Encounter encounter;
 
 	private Obs previousVersion;
+
+	private Integer previousVersionId;
 
 	private Boolean dirty = Boolean.FALSE;
 
@@ -1193,7 +1196,15 @@ public class Obs extends BaseFormRecordableOpenmrsData {
 	 * version of this Obs.
 	 */
 	public Obs getPreviousVersion() {
-		return previousVersion;
+		if (previousVersion != null) {
+			try {
+				previousVersion.getUuid();
+				return previousVersion;
+			} catch (ObjectNotFoundException e) {
+				this.previousVersion = null;
+			}
+		}
+		return null;
 	}
 
 	/**
@@ -1204,6 +1215,28 @@ public class Obs extends BaseFormRecordableOpenmrsData {
 	public void setPreviousVersion(Obs previousVersion) {
 		markAsDirty(this.previousVersion, previousVersion);
 		this.previousVersion = previousVersion;
+		this.previousVersionId = (previousVersion == null) ? null : previousVersion.getObsId();
+	}
+
+	/**
+	 * Gets the database ID of the previous version of this observation.
+	 * This is primarily used by Hibernate for mapping and archiving purposes.
+	 *
+	 * @return the previous version ID
+	 * @since 3.0.0
+	 */
+	public Integer getPreviousVersionId() {
+		return previousVersionId;
+	}
+
+	/**
+	 * Sets the database ID of the previous version of this observation.
+	 *
+	 * @param previousVersionId the previous version ID
+	 * @since 3.0.0
+	 */
+	public void setPreviousVersionId(Integer previousVersionId) {
+		this.previousVersionId = previousVersionId;
 	}
 
 	public Boolean hasPreviousVersion() {
